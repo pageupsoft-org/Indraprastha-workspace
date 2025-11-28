d9d22267418b34d6c51ba120e26a27c589780702
JYOTI YADAV <yadavjyoti806230@gmail.com>
2025-11-28 12:31:40 +0530

    Portal/login loader (#8)
    * add loader on login component
    
    * install npm
    
    * fix import file
    
    * refacter loading funtionlity

--- projects/portal/src/app/pages/login/login.ts
+++ projects/portal/src/app/pages/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, model, OnInit, signal, WritableSignal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
@@ -7,24 +7,29 @@ import { LoginResponse } from '../../core/interface/response/login.response';
 import { Base } from '@portal/core';
 import { ApiRoutes, ErrorHandler, EToastType, ILoginForm, ILoginFormData, localStorageEnum, setLocalStorageItem, ToastService, ValidateControl } from '@shared';
 import { CommonModule } from '@angular/common';
+import { ButtonLoader } from "../../../../../website/src/app/core/component/button-loader/button-loader";
+import { AppLoadingButton } from '@shared';
 
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule,  ValidateControl, CommonModule],
+  imports: [ReactiveFormsModule, ValidateControl, CommonModule, ButtonLoader, AppLoadingButton],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login extends Base {
+export class Login extends Base implements OnInit {
   public loginForm = new FormGroup<ILoginForm>({
     username: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
     password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(8)]),
     fcmToken: new FormControl(''),
   });
   public showPassword: boolean = false
-
-  constructor(private toster: ToastService, private router: Router) {
+   public isBtnLoader: WritableSignal<boolean> = signal(false);
+  constructor(private _toastService: ToastService, private router: Router) {
     super();
+  }
+
+  ngOnInit(): void {
   }
 
   public login() {
@@ -34,25 +39,31 @@ export class Login extends Base {
         password: this.loginForm.controls.password.value || '',
         fcmToken: ""
       };
+      this.isBtnLoader.set(true);
       this.httpPostPromise<IGenericResponse<LoginResponse>, ILoginFormData>(
         ApiRoutes.LOGIN.BASE,
         loginPayload
       )
         .then((response) => {
           if (response && response.data) {
-            this.toster.show({ message: 'Login Successful', duration: 3000, type: EToastType.success });
             setLocalStorageItem(localStorageEnum.token, response.data?.token || '');
             setLocalStorageItem(localStorageEnum.refreshToken, response.data?.refreshToken || '');
+            this._toastService.show({ message: 'Login Successful', duration: 3000, type: EToastType.success });
             this.router.navigate([this.appRoutes.DASHBOARD]);
           }
+          else {
+            this._toastService.show({
+              message: response.errorMessage,
+              type: EToastType.error,
+              duration: 2000,
+            });
+          }
+           this.isBtnLoader.set(true);
         })
-        .catch((error: HttpErrorResponse) => {
-          this.toster.show({
-            message: 'Invalid username or password',
-            duration: 3000,
-            type: EToastType.error,
-          });
-        });
+        .catch(error => {
+         this.isBtnLoader.set(true);
+          //  handle error 
+        })
     } else {
       this.loginForm.markAllAsTouched();
     }
