81be32fdaadbeac18e9b7286afa1d732de1162ec
om-pageup <obhandari@pageupsoft.com>
2025-12-03 15:30:00 +0530

    feat: Move Firebase configuration to a separate file and update FirebaseService to use the new structure

---
+++ projects/shared/src/lib/environments/firebase.env.ts
@@ -0,0 +1,13 @@
+export const firebaseKey = {
+  firebase: {
+    apiKey: 'AIzaSyCAH3JSPltdvmP-AaPG7pJaqlsFe1rCF9A',
+    authDomain: 'mirai-logistics.firebaseapp.com',
+    projectId: 'mirai-logistics',
+    storageBucket: 'mirai-logistics.appspot.com',
+    messagingSenderId: '165717162890',
+    appId: '1:165717162890:web:f73655ac23dfe90369a673',
+    measurementId: 'G-6WPYJKBEGL',
+  },
+  vapidKey:
+    'BGQTV_dtJpCHMOsXIyqHDM9kPUDEG-CkViSu55_BOAcdk2-ewp9EEdXRi4148uNjoNrJFn_X-Fo-uXjQeHGjFM8',
+};
--- projects/shared/src/public-api.ts
+++ projects/shared/src/public-api.ts
@@ -21,6 +21,7 @@ export * from './lib/enum/size.enum';
 export * from './lib/enum/description-type.enum';
 export * from './lib/enum/order-status.enum';
 export * from './lib/environments/dev.env';
+export * from './lib/environments/firebase.env';
 
 export * from './lib/interface/model/header.model';
 export * from './lib/interface/model/login.model';
