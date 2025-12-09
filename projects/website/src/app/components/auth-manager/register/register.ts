e29fdb027b71989e47ec363ffa0cc4e05f794e82
om-pageup <obhandari@pageupsoft.com>
2025-12-09 16:39:17 +0530

    removed field from user register from website part

--- projects/website/src/app/components/auth-manager/register/register.ts
+++ projects/website/src/app/components/auth-manager/register/register.ts
@@ -26,13 +26,13 @@ export class Register implements OnInit {
 
   public registerForm: FormGroup<IRegisterForm> = new FormGroup<IRegisterForm>({
     firstName: new FormControl(null, [Validators.required]),
-    lastName: new FormControl(null, [Validators.required]),
-    email: new FormControl(null, [Validators.required, Validators.email]),
+    lastName: new FormControl(null),
+    email: new FormControl(null),
     contact: new FormControl(null, [
       Validators.required,
       patternWithMessage(/^(?:\+91|91)?[6789]\d{9}$/, 'Enter a valid mobile number.'),
     ]),
-    userName: new FormControl(null, [Validators.required]),
+    userName: new FormControl(null),
     password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
   });
 
@@ -56,7 +56,7 @@ export class Register implements OnInit {
             if (res.data) {
               this.authType.set(EAuthManager.login);
               this._toastService.show({
-                message: 'Login success',
+                message: 'User registered successfully',
                 type: EToastType.success,
                 duration: 2000,
               });
