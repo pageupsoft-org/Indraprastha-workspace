ba2f81b73c5fa6241728c70c26ea63daf63f9763
om-pageup <obhandari@pageupsoft.com>
2025-12-11 12:33:19 +0530

    feat: Implement checkout page with form validation and API integration

--- projects/website/src/app/pages/checkout/checkout.ts
+++ projects/website/src/app/pages/checkout/checkout.ts
@@ -1,23 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { ICheckoutForm, initializeICheckoutForm } from './checkout.model';
-import { ValidateControl } from '@shared';
+import {
+  ICheckoutForm,
+  initializeICheckoutForm,
+  initializeResponseCheckout,
+  IResponseCheckout,
+} from './checkout.model';
+import { ApiRoutes, httpGet, IRGeneric, ValidateControl } from '@shared';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-checkout',
-  imports: [ReactiveFormsModule, ValidateControl],
+  imports: [ReactiveFormsModule, ValidateControl, CommonModule],
   templateUrl: './checkout.html',
   styleUrl: './checkout.scss',
 })
 export class Checkout {
   public checkoutForm: FormGroup<ICheckoutForm> = initializeICheckoutForm();
+  public checkoutData: WritableSignal<IResponseCheckout> = signal(initializeResponseCheckout());
 
-  public onPaymentSumit(){
+  constructor() {
+    this.getCheckoutData();
+  }
+
+  public onPaymentSumit() {
     console.log(this.checkoutForm.value);
-    if(this.checkoutForm.valid){
+    if (this.checkoutForm.valid) {
       // proceed to payment
     } else {
       this.checkoutForm.markAllAsTouched();
     }
+  }
+
+  private getCheckoutData() {
+    httpGet<IRGeneric<IResponseCheckout>>(ApiRoutes.CART.CHECKOUT).subscribe({
+      next: (res) => {
+        if (res?.data) {
+          this.checkoutData.set(res.data);
+        } else {
+          this.checkoutData.set(initializeResponseCheckout());
+        }
+      },
+      error: () => {
+        this.checkoutData.set(initializeResponseCheckout());
+      },
+    });
   }
 }
